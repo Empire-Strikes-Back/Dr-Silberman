0b2de65ab74f79fc6269cf4717f6cb2f51ae372b
ipfs-wing <ipfs-wing@program>
2021-07-30 11:00:08 +0300

    i am ipfs-wing program

---
+++ program.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+repl(){
+  clj -M:repl
+}
+
+push(){
+  ORIGIN=$(git remote get-url origin)
+  rm -rf .git
+  git init -b main
+  git remote add origin $ORIGIN
+  git config --local include.path ../.gitconfig
+  git add .
+  git commit -m "i am ipfs-wing program"
+  git push -f -u origin main
+}
+
+
+"$@"
---
+++ readme.txt
@@ -0,0 +1,3 @@
+i am ipfs-wing program - i conduct experiments for ipfs programs and libraries
+
+i have no git history - my source files define me
